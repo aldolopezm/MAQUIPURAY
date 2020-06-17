6124fcfb2d64d1de822ba9f4f06054959d7e0557
aldolopezm <65749882+aldolopezm@users.noreply.github.com>
2020-06-17 17:23:44 -0500

    APK_V1
    apk version cliente

---
+++ Maquipuray_apk_movil/Maquipuray_apk/app/src/main/java/com/maquipuray/maquipuray_apk/destacados/DescatadosAdapter.java
@@ -0,0 +1,8 @@
+package com.maquipuray.maquipuray_apk.destacados;
+
+/**
+ * Created by maquipuray on 8/06/2020.
+ * Copyright Ⓒ 2020 . All rights reserved.
+ */
+public class DescatadosAdapter {
+}
