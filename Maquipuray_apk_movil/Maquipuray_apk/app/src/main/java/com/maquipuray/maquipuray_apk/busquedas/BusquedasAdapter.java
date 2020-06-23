32c822174004fec7230be58686c162e63b5dac5c
KenzoMoreno <renzo202930@gmail.com>
2020-06-23 13:40:41 -0500

    Proyectos actualizados y renombres de carpetas
    Renombre de carpetas para mejor gestion

--- Maquipuray_apk_movil/Maquipuray_apk/app/src/main/java/com/maquipuray/maquipuray_apk/busquedas/BusquedasAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.maquipuray.maquipuray_apk.busquedas;
-
-/**
- * Created by maquipuray on 8/06/2020.
- * Copyright Ⓒ 2020 . All rights reserved.
- */
-public class BusquedasAdapter {
-}
--- Maquipuray_apk_movil/Maquipuray_apk/app/src/main/java/com/maquipuray/maquipuray_apk/destacados/DescatadosAdapter.java
+++
@@ -1,8 +0,0 @@
-package com.maquipuray.maquipuray_apk.destacados;
-
-/**
- * Created by maquipuray on 8/06/2020.
- * Copyright Ⓒ 2020 . All rights reserved.
- */
-public class DescatadosAdapter {
-}
